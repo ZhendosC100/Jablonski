bfd09e4ba516108dcabd5aa85b8bfd52679f4f17
Yauhen Tretsyak <zhendos@tut.by>
2019-01-30 23:21:40 +0100

    Fixed scrolling whith modal

--- src/parts/modal.js
+++ src/parts/modal.js
@@ -1,23 +1,43 @@
 export default function modal() {
     let modal = document.getElementById('popup_modal'),
+        substrate = document.querySelector('.substrate'),
         body = document.querySelector('body'),
-        input_number = document.querySelector('.number');
+        inputNumber = document.querySelectorAll('.number'),
+        inputName = document.querySelectorAll('.name');
 
-        
-        input_number.addEventListener('input', () => {
-            input_number.value = input_number.value.replace(/[^0-9+]/ig, ''); 
-        });
-        
+    const onlyNumber = () => {
+        for(let i = 0; i < inputNumber.length; i++){
+            inputNumber[i].addEventListener('input', () => {
+                inputNumber[i].value = inputNumber[i].value.replace(/[^0-9+]/ig, ''); 
+            });
+        }
+    };
+
+    const cleanField = () => {
+        for(let i = 0; i < inputName.length; i++){
+            inputName[i].value = ""; 
+        }
+
+        for(let i = 0; i < inputNumber.length; i++){
+            inputNumber[i].value = ""; 
+        }
+    };        
 
     body.addEventListener('click', (e) => {
         let target = e.target;
 
         if(target && target.classList.contains('call') || target.classList.contains('button') || target.classList.contains('button_feedback')){
             modal.classList.add('active');
+            substrate.classList.add('substrate_active');
+            body.classList.add('stop-scrolling'); //Stop scrolling the page
+            onlyNumber();
         }
 
         if(target && target.classList.contains('close')) {
             modal.classList.remove('active');
+            substrate.classList.remove('substrate_active');
+            body.classList.remove('stop-scrolling'); //Start to scrolling the page
+            cleanField();
         }
     });
 }
