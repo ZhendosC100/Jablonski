298529e874deec22c4dcb3a62e16547ebdb290fc
Yauhen Tretsyak <zhendos@tut.by>
2019-02-02 18:14:34 +0100

    Scroll safari

--- src/parts/scroll.js
+++ src/parts/scroll.js
@@ -5,7 +5,10 @@ export default function scroll(){
         promo_link = document.querySelectorAll('.promo_link'),
         feedback_link = document.querySelectorAll('.feedback_link'),
         about_link = document.querySelectorAll('.about_link'),
-        move_up = document.querySelectorAll('.move_up');
+        move_up = document.querySelectorAll('.move_up'),
+        isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification),
+        isIE = /*@cc_on!@*/false || !!document.documentMode,
+        isEdge = !isIE && !!window.StyleMedia;
 
 function scrollBox (name, k) {
     name.forEach(function(item){
@@ -24,9 +27,12 @@ function scrollBox (name, k) {
     });
 }
 
-scrollBox(repair_link, 30);
-scrollBox(promo_link, 120);
-scrollBox(feedback_link, 80);
-scrollBox(about_link, -60);
-scrollBox(move_up, 8);
+if(!isIE || !isEdge || !isSafari){
+    scrollBox(repair_link, 30);
+    scrollBox(promo_link, 120);
+    scrollBox(feedback_link, 80);
+    scrollBox(about_link, -60);
+    scrollBox(move_up, 8);
+}
+
 }
--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,16 @@ module.exports = {
   },
   mode: 'development',
   watch: true,
-  devtool: 'source-map'
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+      }
+    ]
+  }
 };
