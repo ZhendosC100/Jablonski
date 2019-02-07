ffd92ebd17ee168ad6d22118135b216c008e5a3e
Yauhen Tretsyak <zhendos@tut.by>
2019-02-07 14:54:22 +0100

    Fixed 767px, added form backEnd

--- src/parts/modal.js
+++ src/parts/modal.js
@@ -6,6 +6,7 @@ export default function modal() {
         call = document.querySelectorAll('.call'),
         button = document.querySelectorAll('.button'),
         btnFeedback = document.querySelectorAll('.button_feedback'),
+        formPl = document.getElementById('modal_form'),
         inputNumber = document.querySelectorAll('.number'),
         inputName = document.querySelectorAll('.name');
 
@@ -58,25 +59,71 @@ export default function modal() {
         displayModal(btnFeedback[i]);
     }
 
-
     hideModal(close);
+
+    //--------------#Server send data#----------//
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с Вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    },
+        statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+
+    function sendForm(elem) {
+        elem.addEventListener('submit', function (event) {
+            event.preventDefault();
+            elem.appendChild(statusMessage);
+
+            let formData = new FormData(elem);
+
+            let obj = {}; //Вариант отправки через JSON
+            formData.forEach(function (value, key) { //Вариант отправки через JSON
+                obj[key] = value; //Вариант отправки через JSON
+            });
+            let json = JSON.stringify(obj);
+
+            // $.ajax({
+			// 	type: "POST",
+			// 	url: "mailer/smart.php",
+			// 	data: JSON.stringify(obj)
+			// });
+console.log(json);
+            function postData() {
+                
+                return new Promise(function (resolve, reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', './mailer/smart.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8'); //Вариант отправки через JSON        
+                    // request.setRequestHeader ('Content-type', 'application/x-www-form-urlencoded');
+
+                    request.onreadystatechange = function () {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState === 4 && request.readyState == 200) {
+                            resolve();
+                        } else {
+                            reject();
+
+                        }
+                    };
+
+                    request.send(json);
+                });
+
+            } // end of function postData 
+            
+
+            postData(json)
+                .then(() => statusMessage.innerHTML = message.loading)
+                .then(() => statusMessage.innerHTML = message.success)
+                .catch(() => statusMessage.innerHTML = message.failure)
+                .then(cleanField);
+
+        });
+        
+    }
     
-
-// body.addEventListener('click', (e) => {
-//         let target = e.target;
-
-//         if(target && target.classList.contains('call') || target.classList.contains('button') || target.classList.contains('button_feedback')){
-//             modal.classList.add('active');
-//             substrate.classList.add('substrate_active');
-//             body.classList.add('stop-scrolling'); //Stop scrolling the page
-//             onlyNumber();
-//         }
-
-//         if(target && target.classList.contains('close')) {
-//             modal.classList.remove('active');
-//             substrate.classList.remove('substrate_active');
-//             body.classList.remove('stop-scrolling'); //Start to scrolling the page
-//             cleanField();
-//         }
-//     });
+        sendForm(formPl);
+    
 }
